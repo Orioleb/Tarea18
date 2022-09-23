485278b70e6a4a5a9756ca582e6511455c3bcecb
Orioleb <100229248+Orioleb@users.noreply.github.com>
2022-09-23 11:18:28 +0200

    Ejercicio 6

--- Tarea18/src/PiezasyProveedores.java
+++ Tarea18/src/PiezasyProveedores.java
@@ -27,7 +27,16 @@ public class PiezasyProveedores {
 					+ "precio int," + "primary key(codigopiezas,idproveedor)," + "key(codigopiezas),"
 					+ "foreign key(codigopiezas)" + "references piezas(codigo)," + "key(idproveedor),"
 					+ "foreign key(idproveedor)" + "references proveedores(id))");
-			
+			st.executeUpdate("INSERT INTO Piezas VALUE " + "(1,'Martillo')," + "(2,'Metal')," + "(3,'Tuerca'),"
+				+ "(4,'Madera')," + "(5,'Espadrapo')," + "(6,'Cinta Americana')," + "(7,'Cinta Adhesiva')," + "(8,'SuperGlue'),"
+				+ "(9,'Caja')," + "(10,'Sierra')");
+			st.executeUpdate("INSERT INTO Proveedores VALUE" + "('ABC','Ferreteries Aubert')," + "('ABD','Ferreterias PACO'),"
+					+ "('ABE','Ferreterias Juan')," + "('ABF','Ferreterias Tarragona')," + "('ABG','Barcelona'),"
+					+ "('ABH','Ferreterias Reus')," + "('ABI','Hospital Sant Pau')," + "('ABJ','Club Tennis'),"
+					+ "('ABK','Hospital Vall Hebron')," + "('ABL','Universitat del CAmp')");
+			st.executeUpdate("INSERT INTO suministra VALUE" + "(1,'ABC',50)," + "(2,'ABD',65)," + "(3,'ABE',40),"
+				+ "(4,'ABF',15)," + "(5,'ABG',70)," + "(6,'ABH',85)," + "(7,'ABI',35)," + "(8,'ABJ',74),"
+				+ "(9,'ABK',10)," + "(10,'ABL',40)");
 			System.out.println("Taulas creadas");
 			}
 			Properties datos = new Properties();
